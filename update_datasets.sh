99d92e57d3fc4bff25c8f5a0039675b7a42c44e9
Mednikov Yuriy <mednikov.yuriy@gmail.com>
2017-01-02 10:44:15 +0300

    ~ datasets will be sorted ~ update datasets (+23 movies, +2 tv series, +8 tv episodes)

--- update_datasets.sh
+++ update_datasets.sh
@@ -3,20 +3,20 @@ CMD_SUFFIX="TO STDOUT DELIMITER '|' CSV HEADER;\"";
 
 cd datasets;
 wc -l imdb_tv_series.csv;
-time ssh -C sys_1 "$CMD_PREFIX (select imdb_id, title from imdb_tv_series) $CMD_SUFFIX" > imdb_tv_series.csv;
+time ssh -C sys_1 "$CMD_PREFIX (select imdb_id, title from imdb_tv_series) $CMD_SUFFIX" | sort > imdb_tv_series.csv;
 wc -l imdb_tv_series.csv;
 
 
 wc -l imdb_tv_episodes.csv;
-time ssh -C sys_1 "$CMD_PREFIX (select imdb_id, series_id, season_no, episode_no, title from imdb_tv_episodes) $CMD_SUFFIX" > imdb_tv_episodes.csv;
+time ssh -C sys_1 "$CMD_PREFIX (select imdb_id, series_id, season_no, episode_no, title from imdb_tv_episodes) $CMD_SUFFIX" | sort > imdb_tv_episodes.csv;
 wc -l imdb_tv_episodes.csv;
 
 
 wc -l imdb_movies.csv;
-time ssh -C sys_1 "$CMD_PREFIX (select imdb_id, title from imdb_movies) $CMD_SUFFIX" > imdb_movies.csv;
+time ssh -C sys_1 "$CMD_PREFIX (select imdb_id, title from imdb_movies) $CMD_SUFFIX" | sort > imdb_movies.csv;
 wc -l imdb_movies.csv;
 
 
 wc -l torrents.csv;
-time ssh -C sys_1 "$CMD_PREFIX (select distinct on (torrent_id, title) torrent_id, title from torrents_pubs) $CMD_SUFFIX" > torrents.csv;
+time ssh -C sys_1 "$CMD_PREFIX (select distinct on (torrent_id, title) torrent_id, title from torrents_pubs) $CMD_SUFFIX" | sort > torrents.csv;
 wc -l torrents.csv;
